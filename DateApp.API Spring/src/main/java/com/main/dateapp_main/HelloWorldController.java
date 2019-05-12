b9418a9704192a48939334150b430a2205116857
Bhaskar <bhaskarreddydn@gmail.com>
2019-05-12 09:45:56 +0530

    night

--- DateApp.API Spring/src/main/java/com/main/dateapp_main/HelloWorldController.java
+++ DateApp.API Spring/src/main/java/com/main/dateapp_main/HelloWorldController.java
@@ -1,8 +1,11 @@
 package com.main.dateapp_main;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class HelloWorldController {
 	
@@ -10,5 +13,18 @@ public class HelloWorldController {
 	public String helloWorld() {
 		return "Hello World";
 	}
+	
+	@GetMapping(path = "/hellobeans")
+	public HelloBean helloWorldBeans() {
+		return new HelloBean("Hello World");
+	}
+	
+	// /hellobeanspathvar/demo
+	@GetMapping(path = "/hellobeanspathvar/{name}")
+	public HelloBean helloWorldBeansPathVar(@PathVariable String name) {
+		return new HelloBean("Hello World " +name);
+	}
+	
+	
 
 }
